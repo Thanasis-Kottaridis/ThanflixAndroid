5382dcb43c0b50d280d5f138ceee2e1bc7ed203c
Athanasios Kottaridis <tkottaridis@cognity.gr>
2023-08-12 13:19:34 +0300

    Implementation: Create domain Module

--- app/src/main/java/gr/thanflix/MainActivity.kt
+++ app/src/main/java/gr/thanflix/MainActivity.kt
@@ -3,6 +3,8 @@ package gr.thanflix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,6 +12,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import gr.thanflix.domain.utils.ModuleTester
 import gr.thanflix.ui.theme.ThanflixAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,10 +35,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp)
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+
+        Text(
+            text = ModuleTester.testModule(),
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
---
+++ domain/src/main/java/gr/thanflix/domain/utils/ModuleTester.kt
@@ -0,0 +1,8 @@
+package gr.thanflix.domain.utils
+
+object ModuleTester {
+
+    fun testModule(): String {
+        return "Module Works perfectly"
+    }
+}
